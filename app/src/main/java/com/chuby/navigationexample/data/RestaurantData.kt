d1192907aa06784066f5bc86aa24c131078547b7
David Córdova Cardoza <isc.ipn.david@gmail.com>
2018-10-03 11:57:09 -0500

    [MODIFY] - All data classes are now Parcelables

--- app/src/main/java/com/chuby/navigationexample/data/RestaurantData.kt
+++ app/src/main/java/com/chuby/navigationexample/data/RestaurantData.kt
@@ -1,5 +1,9 @@
 package com.chuby.navigationexample.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Restaurant(
         val id: Int,
         val name: String,
@@ -8,22 +12,25 @@ data class Restaurant(
         var image: Image? = null,
         var location: Location? = null,
         var reviews: MutableList<Review> = mutableListOf()
-)
+) : Parcelable
 
+@Parcelize
 data class Image(
         val url: String,
         val width: Int,
         val height: Int
-)
+) : Parcelable
 
+@Parcelize
 data class Location(
         val latitude: Double,
         val longitude: Double
-)
+) : Parcelable
 
+@Parcelize
 data class Review(
         val id: Int,
         val reviewer: String,
         val description: String,
         val rate: Double
-)
+) : Parcelable
