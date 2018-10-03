493909bf8e0b6e3fec7bea9d39c66e2671c020e2
David Córdova Cardoza <isc.ipn.david@gmail.com>
2018-10-03 12:37:35 -0500

    [MODIFY] - Showing review detail, only showing Review#toString

--- app/src/main/java/com/chuby/navigationexample/reviews/ReviewDetailFragment.kt
+++ app/src/main/java/com/chuby/navigationexample/reviews/ReviewDetailFragment.kt
@@ -6,8 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.chuby.navigationexample.R
+import com.chuby.navigationexample.data.Review
+import kotlinx.android.synthetic.main.fragment_review_detail.*
 
 class ReviewDetailFragment : Fragment() {
+
+    private lateinit var review: Review
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        review = ReviewDetailFragmentArgs.fromBundle(arguments).review
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_review_detail, container, false)
@@ -15,6 +24,7 @@ class ReviewDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        tvReviewObject.text = review.toString()
     }
 
 }
