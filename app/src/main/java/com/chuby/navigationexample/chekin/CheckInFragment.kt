5926dc166b449fd995e52b057c1d0a9279ed95d0
David Córdova Cardoza <isc.ipn.david@gmail.com>
2018-10-03 13:19:38 -0500

    [ADD] - CheckIn screen added to navigation graph

---
+++ app/src/main/java/com/chuby/navigationexample/chekin/CheckInFragment.kt
@@ -0,0 +1,19 @@
+package com.chuby.navigationexample.chekin
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chuby.navigationexample.R
+
+class CheckInFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_checkin, container, false)
+    }
+}
