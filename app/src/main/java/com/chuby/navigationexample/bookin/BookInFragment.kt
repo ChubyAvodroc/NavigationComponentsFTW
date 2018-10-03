3b5aedf17efb24af0c1e8b4f727a4b27f7883c2a
David Córdova Cardoza <isc.ipn.david@gmail.com>
2018-10-03 14:41:13 -0500

    [MODIFY] - Navigation from BookIn screen to ReviewScreen, just to show the different stacks for different destinations

--- app/src/main/java/com/chuby/navigationexample/bookin/BookInFragment.kt
+++ app/src/main/java/com/chuby/navigationexample/bookin/BookInFragment.kt
@@ -5,15 +5,19 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.chuby.navigationexample.R
+import kotlinx.android.synthetic.main.fragment_bookin.*
 
 class BookInFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bookin, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bBook.setOnClickListener(Navigation
+                .createNavigateOnClickListener(R.id.action_bookInFragment_to_reviewDetailFragment))
     }
 }
